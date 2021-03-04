4718c0ad4a161a61cb3a058cb2702fc5dac7785b
Samuel Rohrer <srohrer@mitre.org>
2021-03-04 16:41:46 -0500

    netlogo file upload, cleanup

--- src/main/java/org/mitre/base/NetLogoController.java
+++ src/main/java/org/mitre/base/NetLogoController.java
@@ -1,5 +1,8 @@
 package org.mitre.base;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 
@@ -58,16 +62,29 @@ public class NetLogoController {
 	 * @param list of program to store in commands
 	 *
 	 * @return response whether parsed successfully
+	 * @throws IOException
 	 *
 	 */
 	@PostMapping("/netlogo/upload")
 	public ResponseEntity<String> uploadAgentModel(@RequestParam("agent") String agent,
-			                                @RequestParam("file") MultipartFile file) {
+			                 @RequestParam("file") MultipartFile file) throws IOException {
+		// capture the file and turn it into a reader
+		InputStreamReader isr = new InputStreamReader(file.getInputStream(), "UTF-8");
+		BufferedReader rd = new BufferedReader(isr);
 
+		// now use readLine to copy into ArrayList
+		ArrayList<String> nlcode = Lists.newArrayList();
+		String next = rd.readLine();
+		while (next != null) {
+			nlcode.add(next);
+			next = rd.readLine();
+		}
 
+		// add agent + netlogo program pair to global routines
+		routines.put(agent, nlcode);
+
+		// return successful
 		return new ResponseEntity<>("File upload success!", HttpStatus.OK);
 	}
-
-
 
 }
