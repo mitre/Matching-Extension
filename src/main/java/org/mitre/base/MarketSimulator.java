10c2416ba582f3748d217f32b38462c23410aa41
Samuel Rohrer <srohrer@mitre.org>
2021-04-13 14:08:26 -0400

    add netlogo plugin stubs

---
+++ src/main/java/org/mitre/base/MarketSimulator.java
@@ -0,0 +1,122 @@
+/**
+ *
+ */
+package org.mitre.base;
+
+import javax.swing.JPanel;
+
+import org.nlogo.api.NetLogoListener;
+import org.nlogo.core.CompilerException;
+import org.nlogo.window.GUIWorkspace;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author srohrer
+ *
+ */
+@Component
+public class MarketSimulator extends JPanel implements NetLogoListener {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -8256754655837394131L;
+	private transient GUIWorkspace wspace;
+
+
+	/**
+	 *
+	 * @param workspace
+	 */
+	public MarketSimulator(GUIWorkspace workspace) {
+		this.wspace = workspace;
+		wspace.listenerManager.addListener(this);
+	}
+
+	/**
+	 *
+	 */
+	public MarketSimulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void buttonPressed(String buttonName) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void buttonStopped(String buttonName) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void chooserChanged(String name, Object value, boolean valueChanged) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void codeTabCompiled(String text, CompilerException errorMsg) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void commandEntered(String owner, String text, char agentType, CompilerException errorMsg) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void inputBoxChanged(String name, Object value, boolean valueChanged) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void modelOpened(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void possibleViewUpdate() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void sliderChanged(String name, double value, double min, double increment, double max, boolean valueChanged,
+			boolean buttonReleased) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void switchChanged(String name, boolean value, boolean valueChanged) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public void tickCounterChanged(double ticks) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/org/mitre/base/MarketSimulatorTest.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package org.mitre.base;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author srohrer
+ *
+ */
+public class MarketSimulatorTest {
+
+	@Test
+	public void testDefault() {
+		assertEquals(0, 0);
+	}
+
+}
